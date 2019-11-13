2d97daa1189b4b0374ecfcac8ca0794ad6e49f74
lagab.samir <samir.lagab@hei.yncrea.fr>
2019-11-13 12:11:39 +0100

    Résolution de conflit

--- project_iti/src/main/java/hei/devweb/projetit/dao/UtilisateurDao.java
+++ project_iti/src/main/java/hei/devweb/projetit/dao/UtilisateurDao.java
@@ -1,9 +1,9 @@
 package hei.devweb.projetit.dao;
 
+import hei.devweb.projetit.entities.Utilisateur;
+
 public interface UtilisateurDao {
 
-
-
-
+    public Utilisateur getUtilisateur(Integer id);
 
 }
