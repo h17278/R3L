5c4bdd141672d986575d02a18763417f6fa315f0
Gautier Roberval <gautier.roberval@hei.yncrea.fr>
2019-11-19 11:39:40 +0100

    Modification formulaire + UTF-8

--- project_iti/src/main/java/hei/devweb/projetit/controller/GenericServlet.java
+++ project_iti/src/main/java/hei/devweb/projetit/controller/GenericServlet.java
@@ -13,6 +13,7 @@ public abstract class GenericServlet extends HttpServlet {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
 
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
