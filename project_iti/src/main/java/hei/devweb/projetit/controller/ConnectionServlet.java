7ab3f24e4049d7aeed389050b895e79d29a8763d
Victor Lecomte <victor.lecomte@hei.yncrea.fr>
2019-11-20 12:17:06 +0100

    travail sur connection, ajout de l argon2 dans dependencies

--- project_iti/src/main/java/hei/devweb/projetit/controller/ConnectionServlet.java
+++ project_iti/src/main/java/hei/devweb/projetit/controller/ConnectionServlet.java
@@ -23,7 +23,31 @@ public class ConnectionServlet extends GenericServlet {
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+
+        List<Utilisateur> userList = EventService.getInstance().utilisateurList();
+        context.setVariable("userList", userList);
+
         templateEngine.process("connection", context, resp.getWriter());
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String pseudo = req.getParameter("pseudo");
+        String mdp = req.getParameter("mdp");
+
+        List<Utilisateur> userList = EventService.getInstance().utilisateurList();
+        for (int i = 0; i < userList.size(); i++){
+            if (pseudo == userList.get(i).getPseudo() && mdp == userList.get(i).getMotdepasse()) {
+
+                req.getSession().setAttribute("pseudo",pseudo);
+                System.out.println("j'ai recup" + pseudo);
+                req.getSession().setAttribute("president",userList.get(i).getPresident());
+                req.getSession().setAttribute("club",userList.get(i).getClub());
+
+                resp.sendRedirect("home");
+            }
+        }
+
+    }
 }
---
+++ project_iti/src/main/java/hei/devweb/projetit/utils/PasswordUtils.java
@@ -0,0 +1,25 @@
+package hei.devweb.projetit.utils;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+
+public class PasswordUtils {
+    private static final int LONGUEUR_SEL = 128;
+    private static final int LONGUEUR_HASH = 128;
+
+    private static final int NOMBRE_ITERATIONS = 5;
+    private static final int MEMOIRE = 65536;
+    private static final int PARALLELISME = 1;
+
+    private static Argon2 instancierArgon2() {
+        return Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2i, LONGUEUR_SEL, LONGUEUR_HASH);
+    }
+
+    public static String genererMotDePasse(String motDePasse) {
+        return instancierArgon2().hash(NOMBRE_ITERATIONS, MEMOIRE, PARALLELISME, motDePasse);
+    }
+
+    public static boolean validerMotDePasse(String motDePasse, String hashCorrect) {
+        return instancierArgon2().verify(hashCorrect, motDePasse);
+    }
+}
