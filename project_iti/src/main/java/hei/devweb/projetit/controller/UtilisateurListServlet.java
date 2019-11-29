d9fb0b5d65c94eefe55aae3edba388665c1bf396
lagab.samir <samir.lagab@hei.yncrea.fr>
2019-11-29 08:50:11 +0100

    list utilisateur

--- project_iti/src/main/java/hei/devweb/projetit/controller/UtilisateurListServlet.java
+++ project_iti/src/main/java/hei/devweb/projetit/controller/UtilisateurListServlet.java
@@ -8,18 +8,27 @@ import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hei.devweb.projetit.entities.Utilisateur;
 import hei.devweb.projetit.service.UserService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-@WebServlet("/user/list")
+@WebServlet("/user")
 public class UtilisateurListServlet extends GenericServlet {
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
         List<Utilisateur> utilisateurs = UserService.getInstance().listUtilisateur();
         String utilisateursJson = MAPPER.writeValueAsString(utilisateurs);
         resp.getWriter().print(utilisateursJson);
+
+        context.setVariable("user", utilisateursJson);
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+        templateEngine.process("user", context, resp.getWriter());
     }
 }
