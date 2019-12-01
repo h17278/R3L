38b04f127ee7bd451cad84806408911b1182efa9
Pierre Larghero <pierre.larghero@hei.yncrea.fr>
2019-12-02 00:44:28 +0100

    Update CSS

---
+++ project_iti/src/main/java/hei/devweb/projetit/service/LogService.java
@@ -0,0 +1,9 @@
+package hei.devweb.projetit.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogService {
+
+    static final Logger LOGGER = LogManager.getLogger();
+}
